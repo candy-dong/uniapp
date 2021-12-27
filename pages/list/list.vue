d55c3c4103209bb6c6791e710cb2b02e2eae322a
candy-dong <704483696@qq.com>
2021-12-27 14:15:59 +0800

    下拉刷新

---
+++ pages/list/list.vue
@@ -0,0 +1,32 @@
+<template>
+	<view>
+		<view>这是列表页</view>
+		<view v-for="item in list">{{item}}</view>
+		<button type="default" @click="pullDown">下拉刷新</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list: ['前端','java','大数据','UI','测试']
+			}
+		},
+		onPullDownRefresh(){
+			console.log('触发了下拉刷新')
+			this.list = ['java','大数据','UI','测试','前端']
+			setTimeout(function () {
+				uni.stopPullDownRefresh();
+			}, 2000);
+		},
+		methods: {
+			pullDown() {
+				uni.startPullDownRefresh()
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
