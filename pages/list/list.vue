dd2decda5988ef30491e7746b3fde0708d82f140
candy-dong <704483696@qq.com>
2021-12-27 14:43:29 +0800

    上拉加载

--- pages/list/list.vue
+++ pages/list/list.vue
@@ -1,8 +1,8 @@
 <template>
 	<view>
 		<view>这是列表页</view>
-		<view v-for="item in list">{{item}}</view>
-		<button type="default" @click="pullDown">下拉刷新</button>
+		<view class="box-item" v-for="item in list">{{item}}</view>
+		<!-- <button type="default" @click="pullDown">下拉刷新</button> -->
 	</view>
 </template>
 
@@ -10,15 +10,19 @@
 	export default {
 		data() {
 			return {
-				list: ['前端','java','大数据','UI','测试']
+				list: ['前端','java','大数据','UI','测试','UI','测试','前端','java','大数据','UI','测试','前端']
 			}
 		},
 		onPullDownRefresh(){
 			console.log('触发了下拉刷新')
-			this.list = ['java','大数据','UI','测试','前端']
+			this.list = ['java','大数据','UI','测试','前端','java','大数据','UI','测试','前端']
 			setTimeout(function () {
 				uni.stopPullDownRefresh();
 			}, 2000);
+		},
+		onReachBottom() {
+			console.log('页面触底了')
+			this.list = [...this.list,...['java','大数据','UI','测试','前端','java','大数据']]
 		},
 		methods: {
 			pullDown() {
@@ -29,4 +33,8 @@
 </script>
 
 <style>
+	.box-item {
+		width: 100px;
+		line-height: 100px;
+	}
 </style>
